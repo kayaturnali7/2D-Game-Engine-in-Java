c7afebd946d19f80ec7086ee8749ccccbf36a8f6
kayaturnali7 <kayaturnali@icloud.com>
2026-09-20 22:02:09 -0400

    made render engine render entities then ui

--- src/main/java/engine/graphics/RenderEngine.java
+++ src/main/java/engine/graphics/RenderEngine.java
@@ -2,6 +2,7 @@ package engine.graphics;
 
 import engine.entity.Entity;
 import engine.scene.Scene;
+import engine.ui.UserInterface;
 
 import java.awt.Graphics2D;
 import java.util.ArrayList;
@@ -14,8 +15,14 @@ public class RenderEngine {
 
     private void drawEntities(Graphics2D g2d, Scene scene){
         ArrayList<Entity> entities = scene.getEntities();
+        ArrayList<UserInterface> userInterfaces = scene.getUserInterfaces();
+
         for (Entity entity : entities){
             entity.draw(g2d);
+        }
+
+        for (UserInterface userInterface : userInterfaces){
+            userInterface.draw(g2d);
         }
     }
 }
