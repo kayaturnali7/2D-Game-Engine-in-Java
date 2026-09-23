ed0b9c4a238c24dd216e965a8362b3a77c2e765b
kayaturnali7 <kayaturnali@icloud.com>
2026-09-22 21:33:13 -0400

    changed engine entry point

--- src/main/java/asteroids/AsteroidsGame.java
+++ src/main/java/asteroids/AsteroidsGame.java
@@ -7,12 +7,12 @@ import asteroids.scenes.MenuScene;
 
 import java.util.ArrayList;
 
-public class Main {
+public class AsteroidsGame {
     public static void main(String[] args){
         ArrayList<Scene> scenes = new ArrayList<>();
         scenes.add(new GameScene());
         scenes.add(new MenuScene());
 
-        Engine.startEngine(scenes);
+        new Engine(scenes);
     }
 }
